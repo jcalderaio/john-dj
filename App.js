57e2a0b4125faea5040279b7c75f2f5c36e1bea6
John Calderaio <jcalderaio@gmail.com>
2021-02-20 18:07:39 -0500

    First Push

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+import * as Contacts from 'expo-contacts';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const [contact, setContact] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Emails],
+        });
+
+        if (data.length > 0) {
+          setContact(data);
+          setLoading(false);
+          console.log(contact);
+        }
+      }
+    })();
+  }, [contact]);
+
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <SafeAreaView style={{ backgroundColor: 'black' }} />
+      <View style={{ flex: 1, backgroundColor: 'white' }}>
+        {isLoading ? (
+          <View
+            style={{
+              ...StyleSheet.absoluteFill,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <ActivityIndicator size="large" color="black" />
+          </View>
+        ) : null}
+      </View>
     </View>
   );
 }
@@ -14,8 +63,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: Constants.statusBarHeight,
+    marginHorizontal: 16,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 32,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
   },
 });
